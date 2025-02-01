e87e20675277f5189ee3eadfb6673132a4680533
Grandmaster <tamirlan.ramazanov@gmail.com>
2025-02-01 19:12:48 +0500

    step 13 Preparing for Todo List Project status filter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
             <AppHeader />
             <SearchPanel />
             <TodoList todos = {todoData} />
+            <ItemStatusFilter />
         </div>
     );
 }
