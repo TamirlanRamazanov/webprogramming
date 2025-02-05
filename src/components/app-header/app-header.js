b114294b601866188a3d3d0c318fb99a3cb33d91
Grandmaster <tamirlan.ramazanov@gmail.com>
2025-02-05 14:25:28 +0500

    step 17 Editing Project directories

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import './app-header/app-header.css';
+import './app-header.css';
 
 const AppHeader = ({toDo, done}) => {
     return (
         <div className={"app-header d-flex"}>
-            <h1>Todo List</h1>
+            <h1>To-Do List</h1>
             <h2>{toDo} more to do, {done} </h2>
         </div>
     );
