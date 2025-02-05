c53979f4693b555502f18fe19aa2333d362d7273
Grandmaster <tamirlan.ramazanov@gmail.com>
2025-02-05 14:34:20 +0500

    step 19 Use Constructor

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,42 @@
-import React from "react"
+import React, {Component} from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
+export default class TodoListItem extends Component {
+    constructor() {
+        super();
+        this.onLabelClick = () => {
+            console.log(`Done: ${this.props.label}`);
+        };
+    }
 
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+    render() {
 
-    return (
-        <span className="todo-list-item">
+        const {label, important = false} = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+        return (
+            <span className="todo-list-item">
       <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={ this.onLabelClick }>
         {label}
       </span>
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
+        <i className="fa fa-exclamation"/>
       </button>
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
+        <i className="fa fa-trash-o"/>
       </button>
     </span>
-    );
+        );
 
-};
+    };
 
 
-export default TodoListItem;
+}
