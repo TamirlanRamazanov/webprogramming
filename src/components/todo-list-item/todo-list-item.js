a59502043300be59a724833e522773aa0f7fe5c4
Grandmaster <tamirlan.ramazanov@gmail.com>
2025-02-12 13:22:17 +0500

    step 25 deleting elements setState and deleteItem func.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -12,7 +12,7 @@ export default class TodoListItem extends Component {
     onLabelClick = () => {
         this.setState( ({done}) => {
             return {
-                done: true
+                done: !done
             };
         });
         };
